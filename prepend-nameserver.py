dc28cc1f2d08acbe1f0d9e03c729be160d3ed3d5
cms <cms@beatworm.co.uk>
2015-11-12 13:20:47 +0000

    initial checkin
    reads from environment
    prepends nameservers if they're not already there
    writes a resolv.conf for dnsmasq to refer to

---
+++ prepend-nameserver.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+import sys
+import os
+
+
+prepend_nameservers = [ '172.20.0.1' ]
+connection = os.getenv('CONNECTION_UUID')
+
+[iface, evt] = sys.argv[1:]
+
+#nm provides the resolver nameservers list in an env variable
+def get_ns_list(spaced_str=None):
+    if spaced_str == None:
+        spaced_str = os.getenv('IP4_NAMESERVERS')
+    return spaced_str.split(' ')
+
+
+if evt == 'dhcp4-change' and iface == 'wlan0' :
+    nameservers = get_ns_list()
+    if nameservers[0] != prepend_nameservers[0]:
+        new_nameservers = prepend_nameservers + nameservers
+        # subprocess.call(["nmcli", "con", "mod", connection, "ipv4.dns", ','.join(new_nameservers)])
+        with open('/var/lib/lxc/upstream-resolv.conf','w') as upstream:
+            upstream.writelines( "{}\n".format(server) for server in nameservers )
+    print (new_nameservers)
